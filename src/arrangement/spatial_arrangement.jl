055f5162f342d2d50bfa8a4c96ccb202a809e79a
Ennio Malvati <enniohomero@hotmail.it>
2022-06-10 16:42:52 +0200

    Aggiornamento file spatial_arrangement.jl

--- src/arrangement/spatial_arrangement.jl
+++ src/arrangement/spatial_arrangement.jl
@@ -24,123 +24,129 @@ end
 
 `sigma` face fragmentation against faces in `sp_idx[sigma]`
 """
-function frag_face(V, EV, FE, sp_idx, sigma)
+function frag_face(V::Matrix{Float64}, EV::SparseMatrixCSC{Int8, Int64}, FE::SparseMatrixCSC{Int8, Int64},
+    sp_idx::Vector{Int64}, sigma::Int64)
 
-    vs_num = size(V, 1)
+
+    vs_num::Int64 = size(V, 1)
 
 	# 2D transformation of sigma face
-    sigmavs = (abs.(FE[sigma:sigma,:]) * abs.(EV))[1,:].nzind
-    sV = V[sigmavs, :]
-    sEV = EV[FE[sigma, :].nzind, sigmavs]
-    M = Lar.Arrangement.submanifold_mapping(sV)
-    tV = ([V ones(vs_num)]*M)[:, 1:3]  # folle convertire *tutti* i vertici
+    sigmavs::Vector{Int64} = (abs.(FE[sigma:sigma,:])*abs.(EV))[1,:].nzind
+
+    sV::Matrix{Float64} = V[sigmavs, :]
+    sEV::SparseMatrixCSC{Int8, Int64} = EV[FE[sigma, :].nzind, sigmavs]
+    M::Matrix{Float64} = Lar.Arrangement.submanifold_mapping(sV)
+   @views tV::Matrix{Float64} = ([V ones(vs_num)]*M)[:, 1:3]
     sV = tV[sigmavs, :]
+
     # sigma face intersection with faces in sp_idx[sigma]
-    for i in sp_idx[sigma]
-        tmpV, tmpEV = Lar.Arrangement.face_int(tV, EV, FE[i, :])
-		sV, sEV
+    Threads.@threads for i in sp_idx[sigma]
+        tmpV::Matrix{Any}, tmpEV::SparseMatrixCSC{Int8, Int64} = Lar.Arrangement.face_int(tV, EV, FE[i, :])
         sV, sEV = Lar.skel_merge(sV, sEV, tmpV, tmpEV)
     end
 
     # computation of 2D arrangement of sigma face
     sV = sV[:, 1:2]
-    nV, nEV, nFE = planar_arrangement(sV, sEV, sparsevec(ones(Int8, length(sigmavs))))
-    if nV == nothing ## not possible !! ... (each original face maps to its decomposition)
-        return [], spzeros(Int8, 0,0), spzeros(Int8, 0,0)
-    end
-    nvsize = size(nV, 1)
-    nV = [nV zeros(nvsize) ones(nvsize)]*inv(M)[:, 1:3] ## ????
+    nV::Matrix{Float64}, nEV::SparseMatrixCSC{Int8, Int64}, nFE::SparseMatrixCSC{Int8, Int64} = planar_arrangement(sV, sEV, sparsevec(ones(Int8, length(sigmavs))))
+
+    nvsize::Int64 = size(nV, 1)
+    nV = [nV zeros(nvsize) ones(nvsize)]*inv(M)[:, 1:3]
 
     return nV, nEV, nFE
 end
 
 function merge_vertices(V::Lar.Points, EV::Lar.ChainOp, FE::Lar.ChainOp, err=1e-4)
-   vertsnum = size(V, 1)
-   edgenum = size(EV, 1)
-   facenum = size(FE, 1)
-   newverts = zeros(Int, vertsnum)
-   # KDTree constructor needs an explicit array of Float64
-   V = Array{Float64,2}(V)
-   W = convert(Lar.Points, LinearAlgebra.transpose(V))
-   kdtree = KDTree(W)
-# remove vertices congruent to a single representative
-   todelete = []
-   i = 1
-   for vi in 1:vertsnum
-       if !(vi in todelete)
-           nearvs = Lar.inrange(kdtree, V[vi, :], err)
-           newverts[nearvs] .= i
-           nearvs = setdiff(nearvs, vi)
-           todelete = union(todelete, nearvs)
-           i = i + 1
-       end
-   end
-   nV = V[setdiff(collect(1:vertsnum), todelete), :]
-   V[30,:] = V[26,:] #<<<<<<<<<<<<<<<<<< TEST
+    vertsnum = size(V, 1)
+    edgenum = size(EV, 1)
+    facenum = size(FE, 1)
+    newverts = zeros(Int, vertsnum)
 
-   # translate edges to take congruence into account
-   edges = Array{Tuple{Int, Int}, 1}(undef, edgenum)
-   oedges = Array{Tuple{Int, Int}, 1}(undef, edgenum)
-   for ei in 1:edgenum
-       v1, v2 = EV[ei, :].nzind
-       edges[ei] = Tuple{Int, Int}(sort([newverts[v1], newverts[v2]]))
-       oedges[ei] = Tuple{Int, Int}(sort([v1, v2]))
-   end
-   nedges = union(edges)
-   # remove edges of zero length
-   nedges = filter(t->t[1]!=t[2], nedges)
-   nedgenum = length(nedges)
-   nEV = spzeros(Int8, nedgenum, size(nV, 1))
+    # KDTree constructor needs an explicit array of Float64
+    V = Array{Float64,2}(V)
+    W = convert(Lar.Points, LinearAlgebra.transpose(V))
+    kdtree = KDTree(W)
 
-   etuple2idx = Dict{Tuple{Int, Int}, Int}()
-   for ei in 1:nedgenum
-   	begin
-       	nEV[ei, collect(nedges[ei])] .= 1
-       	nEV
-       end
-       etuple2idx[nedges[ei]] = ei
-   end
-   for e in 1:nedgenum
-   	v1,v2 = findnz(nEV[e,:])[1]
-   	nEV[e,v1] = -1; nEV[e,v2] = 1
-   end
+    # remove vertices congruent to a single representative
+    todelete = []
+    i = 1
 
-   # compute new faces to take congruence into account
-   faces = [[
-       map(x->newverts[x], FE[fi, ei] > 0 ? oedges[ei] : reverse(oedges[ei]))
-       for ei in FE[fi, :].nzind
-   ] for fi in 1:facenum]
+    for vi in 1:vertsnum #non è possibile utilizzare @async
+        if !(vi in todelete)
+            nearvs =  Lar.inrange(kdtree, V[vi, :], err)
+            newverts[nearvs] .= i
+            nearvs =  setdiff(nearvs, vi)
+            todelete = union(todelete, nearvs)
+            i = i + 1
+        end
+    end
+    nV = V[setdiff(collect(1:vertsnum), todelete), :]
+
+    # translate edges to take congruence into account
+    edges = Array{Tuple{Int, Int}, 1}(undef, edgenum)
+    oedges = Array{Tuple{Int, Int}, 1}(undef, edgenum)
+
+    for ei in 1:edgenum #non è possibile utilizzare @async
+        v1, v2 = EV[ei, :].nzind
+        edges[ei] = Tuple{Int, Int}(sort([newverts[v1], newverts[v2]]))
+        oedges[ei] = Tuple{Int, Int}(sort([v1, v2]))
+    end
+
+    nedges = union(edges)
+    # remove edges of zero length
+    nedges = filter(t->t[1]!=t[2], nedges)
+    nedgenum = length(nedges)
+    nEV = spzeros(Int8, nedgenum, size(nV, 1))
+
+    etuple2idx = Dict{Tuple{Int, Int}, Int}()
+
+    for ei in 1:nedgenum #non è possibile utilizzare @async
+        begin
+            nEV[ei, collect(nedges[ei])] .= 1
+            nEV
+        end
+        etuple2idx[nedges[ei]] = ei
+    end
+
+    @async for e in 1:nedgenum
+        v1,v2 = findnz(nEV[e,:])[1]
+        nEV[e,v1] = -1; nEV[e,v2] = 1
+    end
+
+    # compute new faces to take congruence into account
+    faces = [[
+        map(x->newverts[x], FE[fi, ei] > 0 ? oedges[ei] : reverse(oedges[ei]))
+        for ei in FE[fi, :].nzind
+    ] for fi in 1:facenum]
 
 
-   visited = []
-   function filter_fn(face)
+    visited = []
+    function filter_fn(face)
 
-       verts = []
-       map(e->verts = union(verts, collect(e)), face)
-       verts = Set(verts)
+        verts = []
+        map(e->verts = union(verts, collect(e)), face)
+        verts = Set(verts)
 
-       if !(verts in visited)
-           push!(visited, verts)
-           return true
-       end
-       return false
-   end
+        if !(verts in visited)
+            push!(visited, verts)
+            return true
+        end
+        return false
+    end
 
-   nfaces = filter(filter_fn, faces)
+    nfaces = filter(filter_fn, faces)
 
-   nfacenum = length(nfaces)
-   nFE = spzeros(Int8, nfacenum, size(nEV, 1))
+    nfacenum = length(nfaces)
+    nFE = spzeros(Int8, nfacenum, size(nEV, 1))
 
-   for fi in 1:nfacenum
-       for edge in nfaces[fi]
-           ei = etuple2idx[Tuple{Int, Int}(sort(collect(edge)))]
-           nFE[fi, ei] = sign(edge[2] - edge[1])
-       end
-   end
+    @async for fi in 1:nfacenum
+        @async for edge in nfaces[fi]
+            ei = etuple2idx[Tuple{Int, Int}(sort(collect(edge)))]
+            nFE[fi, ei] = sign(edge[2] - edge[1])
+        end
+    end
 
-   return Lar.Points(nV), nEV, nFE
-end
-
+    return Lar.Points(nV), nEV, nFE
+ end
 #function merge_vertices(V::Lar.Points, EV::Lar.ChainOp, FE::Lar.ChainOp, err=1e-4)
 #println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 #println("\n\nHERE TO Make LOCAL CONGRUENCE\n\n")
@@ -154,6 +160,7 @@ function spatial_arrangement_1(
 		copEV::Lar.ChainOp,
 		copFE::Lar.ChainOp, multiproc::Bool=false)
 
+
 	# spaceindex computation
 	FV = Lar.compute_FV( copEV, copFE )
 	model = (convert(Lar.Points,V'), FV)
@@ -161,9 +168,9 @@ function spatial_arrangement_1(
 
 	# initializations
 	fs_num = size(copFE, 1)
-    rV = Array{Float64,2}(undef,0,3)
-    rEV = SparseArrays.spzeros(Int8,0,0)
-    rFE = SparseArrays.spzeros(Int8,0,0)
+  rV = Array{Float64,2}(undef,0,3)
+  rEV = SparseArrays.spzeros(Int8,0,0)
+  rFE = SparseArrays.spzeros(Int8,0,0)
 
 	# multiprocessing of face fragmentation
     if (multiproc == true)
@@ -186,27 +193,17 @@ function spatial_arrangement_1(
         end
     else
 	# sequential (iterative) processing of face fragmentation
-        for sigma in 1:fs_num
-            println("\n",sigma, "/", fs_num)
-            nV, nEV, nFE = Lar.Arrangement.frag_face(V, copEV, copFE, sp_idx, sigma)
-# 			v = size(nV,1); e = nEV.m; f = nFE.m
-# @show v-e+f
-# 			if v-e+f > 1
-# 				g = v-e+f - 1	# number of inner loops
-# 				nFE = removeinnerloops(g, nFE)
-# @show nV;
-# @show SparseArrays.nzind(nEV)
-# @show SparseArrays.nzind(nFE)
-# 			end
-			#nV, nEV, nFE = Lar.fragface(V, copEV, copFE, sp_idx, sigma)
-			nV = convert(Lar.Points, nV)
-            a,b,c = Lar.skel_merge( rV,rEV,rFE,  nV,nEV,nFE )
-            rV=a;  rEV=b;  rFE=c
+        @async for sigma in 1:fs_num
+            #println("\n",sigma, "/", fs_num)
+            nV, nEV, nFE = frag_face(V, copEV, copFE, sp_idx, sigma)
+		      	nV = convert(Lar.Points, nV)
+            rV,rEV, rFE= Lar.skel_merge( rV,rEV,rFE,  nV,nEV,nFE )
+            #rV=a;  #rEV=b;  #rFE=c
         end
     end
 	# merging of close vertices, edges and faces (3D congruence)
-	rV, rEV, rFE = merge_vertices(rV, rEV, rFE, err=1e-3)
-    return rV, rEV, rFE
+	rV, rEV, rFE = merge_vertices(rV, rEV, rFE)
+  return rV, rEV, rFE
 end
 
 """
